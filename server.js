27d75ca1fbd409b70102795f18097550bf3be1cb
Ravi Prakash <raviprakash01a@gmail.com>
2020-06-06 13:25:26 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+app.use("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Ravi Prakash",
+  });
+});
+
+
+app.listen("3000", () => {
+  console.log(`server is runing at 3000`);
+});
