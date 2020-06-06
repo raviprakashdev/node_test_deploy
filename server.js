247b816894f26834d4799ecfea578bd1910780b1
Ravi Prakash <raviprakash01a@gmail.com>
2020-06-06 13:36:03 +0530

    home route

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-
 app.use("/", (req, res, next) => {
   res.status(200).json({
-    message: "Ravi Prakash",
+    message: "Ravi Prakash node server running",
   });
 });
-
 
 app.listen("3000", () => {
   console.log(`server is runing at 3000`);
